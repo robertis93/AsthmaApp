90a68845384f28d0a040fcd417c3e35e44b69748
Robert <robertis93@yandex.ru>
2021-11-16 01:05:45 +0500

    java.lang.NumberFormatException com.rightname.asthmaapp.viewmodel.viewModels.MedicamentViewModel$addMedicamentInfo -

--- app/src/main/java/com/example/asthmaapp/view/fragments/MedicamentFragment.kt
+++ app/src/main/java/com/example/asthmaapp/view/fragments/MedicamentFragment.kt
@@ -2,10 +2,10 @@ package com.example.asthmaapp.view.fragments
 
 import android.content.Context
 import android.os.Bundle
-import android.text.method.DigitsKeyListener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -16,6 +16,8 @@ import com.example.asthmaapp.viewmodel.viewModels.MedicamentViewModel
 import kotlinx.coroutines.launch
 
 class MedicamentFragment : BaseFragment<MedicamentFragmentBinding>() {
+    val blockCharacterSet ="1234567890"
+
 
     private val medicamentViewModel: MedicamentViewModel by lazy {
         ViewModelProvider(this).get(MedicamentViewModel::class.java)
@@ -28,8 +30,25 @@ class MedicamentFragment : BaseFragment<MedicamentFragmentBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         setInitMedicament()
-        checkingEnableButton(binding.medicamentNameEditText, binding.medicamentDoseEditText, binding.saveBtn)
-        binding.medicamentDoseEditText.keyListener = DigitsKeyListener.getInstance("0123456789")
+        checkingEnableButton(
+            binding.medicamentNameEditText,
+            binding.medicamentDoseEditText,
+            binding.saveBtn
+        )
+        binding.medicamentDoseEditText.doAfterTextChanged{s ->
+
+                if (s != null) {
+                    for (i in 1..s.length) {
+                        if (!blockCharacterSet.contains(s[i-1])) {
+                                binding.medicamentDoseEditText.setText("")
+                            binding.medicamentDoseLayoutEditText.prefixText = "Введите целое число"
+                        }
+                        else {
+                            binding.medicamentDoseLayoutEditText.prefixText = ""
+                        }
+                    }
+                }
+            }
 
 
         binding.saveBtn.setOnClickListener {
@@ -41,6 +60,7 @@ class MedicamentFragment : BaseFragment<MedicamentFragmentBinding>() {
             hideKeyboard.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
             findNavController().navigate(R.id.action_medicalFragment_to_alarmFragment)
         }
+
     }
 
     private fun setInitMedicament() {
@@ -50,7 +70,11 @@ class MedicamentFragment : BaseFragment<MedicamentFragmentBinding>() {
                 binding.medicamentDoseEditText.setText(medicamentInfo.dose.toString())
                 binding.medicamentNameEditText.setText(medicamentInfo.name)
             } else {
-                checkingEnableButton(binding.medicamentNameEditText, binding.medicamentDoseEditText, binding.saveBtn)
+                checkingEnableButton(
+                    binding.medicamentNameEditText,
+                    binding.medicamentDoseEditText,
+                    binding.saveBtn
+                )
             }
         }
     }
